8188f1b68fc1b813d097a5981d7c66b1b15b74e6
DaviGauze <davigauze08@gmail.com>
2026-01-23 15:40:46 -0300

    Aplicação Inicial finalizada onde o Script leva o mouse até o chamado precisando apenas clicar para pegar o chamado (próxima implementação será o autoclick)

---
+++ main.py
@@ -0,0 +1,26 @@
+import pyautogui
+import time
+
+CONFIDENCE_LEVEL = 0.7 
+CHECK_INTERVAL = 2
+
+def pegar_chamado():
+    print("Monitorando novos chamados no Desk Manager...")
+    while True:
+        try:
+            target = pyautogui.locateCenterOnScreen('status_novo.png', confidence=CONFIDENCE_LEVEL)
+            
+            if target:
+                pyautogui.moveTo(target, duration=0.1)
+                print(f"Movi o mouse para {target}. Ele está em cima do botão?")
+                
+        except pyautogui.ImageNotFoundException:
+            print(".", end="", flush=True) 
+            
+        except KeyboardInterrupt:
+            print("\nAutomação interrompida.")
+            break
+            
+        time.sleep(CHECK_INTERVAL)
+
+pegar_chamado()
