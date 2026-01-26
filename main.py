37bf81f32c6833ecb3efd1039f3f6cda0b8806e7
DaviGauze <davigauze08@gmail.com>
2026-01-26 10:40:23 -0300

    Solucionado erro ao parar a aplicação e alterado logs da aplicação para melhor entendimento do usuário

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ CHECK_INTERVAL = 2
 
 def pegar_chamado():
     print("Monitorando novos chamados no Desk Manager...")
-    while True:
-        try:
-            target = pyautogui.locateCenterOnScreen('status_novo.png', confidence=CONFIDENCE_LEVEL)
-            
-            if target:
-                pyautogui.moveTo(target, duration=0.1)
-                print(f"Mouse movido para {target}.")
+    try:
+        while True:
+            try:
+                target = pyautogui.locateCenterOnScreen('status_novo.png', confidence=CONFIDENCE_LEVEL)
                 
-        except pyautogui.ImageNotFoundException:
-            print(".", end="", flush=True) 
-            
-        except KeyboardInterrupt:
-            print("\nAutomação interrompida.")
-            break
-            
-        time.sleep(CHECK_INTERVAL)
+                if target:
+                    pyautogui.moveTo(target, duration=0.1)
+                    print(f"Chamado encontrado, Mouse movido para {target}.")
+                    
+            except pyautogui.ImageNotFoundException:
+                print("Procurando novos chamados.\n", end="", flush=True) 
 
+            time.sleep(CHECK_INTERVAL)
+                
+    except KeyboardInterrupt:
+        print("\nParando a busca por chamados.")              
+            
 pegar_chamado()
